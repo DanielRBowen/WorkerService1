9ca80305d18e580531d1218d917f62fa28e9f403
Daniel <daniel@bpapos.com>
2022-03-22 16:31:59 -0600

    Made it possible to build as WebHost if run ServiceBase.Run(new ServiceBase[] { webHostService }); when running as a service.

--- WorkerService1/Program.cs
+++ WorkerService1/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
 using Microsoft.Extensions.Hosting.WindowsServices;
+using System.Diagnostics;
+using System.ServiceProcess;
 using WorkerService1;
 
 // To get the windows background service to work successfully,
@@ -12,7 +15,17 @@ using WorkerService1;
 // When publishing: set deployment-mode to Self-contained and check the produce single file and enable ReadyToRun compilation boxes
 // as described here: https://docs.microsoft.com/en-us/dotnet/core/extensions/windows-service
 
-bool isWebHost = false;
+bool isWebHost = true;
+
+//See https://www.stevejgordon.co.uk/running-net-core-generic-host-applications-as-a-windows-service
+var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+if (isService)
+{
+    var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+    var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+    Directory.SetCurrentDirectory(pathToContentRoot);
+}
 
 if (isWebHost)
 {
@@ -31,7 +44,15 @@ if (isWebHost)
             });
         });
 
-    await webHost.Build().RunAsync();
+    if (isService)
+    {
+        var webHostService = new WebHostService(webHost.Build());
+        ServiceBase.Run(new ServiceBase[] { webHostService });
+    }
+    else
+    {
+        await webHost.Build().RunAsync();
+    }
 }
 else
 {
